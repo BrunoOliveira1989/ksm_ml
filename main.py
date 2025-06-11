e522057b9892995530adf2eb46c5d90c0f7f4dfe
Bruno Gomes de Oliveira <bruno.oliveira234@fatec.sp.gov.br>
2025-06-11 19:46:53 -0300

    Atualizando para versão 2.5

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="API Unificada: Chance de Compra + Recomendação")
 app.include_router(clientes.router, prefix="/clientes", tags=["Clientes"])
 app.include_router(produtos.router, prefix="/produtos", tags=["Produtos"])
 
-#if __name__ == "__main__":
-#    import uvicorn
-#    port = int(os.environ.get("PORT", 8000))  # pega a porta da variável de ambiente ou usa 8000 por padrão
-#    uvicorn.run(app, host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # pega a porta da variável de ambiente ou usa 8000 por padrão
+    uvicorn.run(app, host="0.0.0.0", port=port)
